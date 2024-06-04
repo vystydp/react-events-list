99dc7841193f693093ce65f61b5494f8305a8a41
Petr <vystydp@gmail.com>
2024-06-04 22:47:46 +0200

    Add Next.js (#4)
    Adding server

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'React App',
+  description: 'Web site created with Next.js.',
+}
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  )
+}
