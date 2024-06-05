66cdcc5b2cc527b9368d18bb191b7bc0b794d674
Petr <vystydp@gmail.com>
2024-06-05 10:59:27 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,38 @@
 import ClientWrapper from '../components/ClientWrapper';
-import { Event } from '../types/event'
+
+type Event = {
+  id: string;
+  title: string;
+  date: string;
+  description: string;
+};
+
+const getBaseUrl = () => {
+  if (process.env.NEXT_PUBLIC_API_BASE_URL) {
+    return `https://${process.env.NEXT_PUBLIC_API_BASE_URL}`;
+  } else {
+    return `http://${process.env.NEXT_PUBLIC_API_BASE_URL_LOCAL}`;
+  }
+};
 
 async function fetchEvents(): Promise<Event[]> {
-  var url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/events`;
-  console.log(`Fetching events from: ${url}`); // Debug log
-  const res = await fetch(url);
+  const baseUrl = getBaseUrl();
+  console.log(`Fetching events from: ${baseUrl}/api/events`); // Debug log
+  const res = await fetch(`${baseUrl}/api/events`);
   if (!res.ok) {
-    throw new Error(`Failed to fetch events from ${url}: ${res.statusText}`);
+    throw new Error(`Failed to fetch events from ${baseUrl}/api/events: ${res.statusText}`);
   }
   try {
     return await res.json();
   } catch (error) {
-    throw new Error(`Failed to parse JSON from ${url}: ${error.message}`);
+    throw new Error(`Failed to parse JSON from ${baseUrl}/api/events: ${error.message}`);
   }
 }
 
 export default async function Page() {
+  console.log('Building Page component'); // Debug log
   const initialEvents = await fetchEvents();
+  console.log('Fetched initial events:', initialEvents); // Debug log
   
   return (
     <ClientWrapper initialEvents={initialEvents} />
