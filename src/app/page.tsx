664b3ffcc27d803b2e662b732545f077b445b39d
Petr <vystydp@gmail.com>
2024-06-05 12:10:26 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 import ClientWrapper from '../components/ClientWrapper';
+import { Event } from '../types/event'
 
 export default async function Page() {
   console.log('Building Page component'); // Debug log
-  const initialEvents = [];
+  let initialEvents: Event[] = [{ 
+    id: "1", 
+    title: "Interview with Slido / Panaxeo", 
+    date: "2024-06-05T02:00:40+00:00", 
+    description: "Interview" ,
+    location: {
+      long: 10,
+      lat: 10
+    }
+  }];
   console.log('Fetched initial events:', initialEvents); // Debug log
   
   return (
