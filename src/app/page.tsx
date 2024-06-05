76896e9a13e81151ba68c088d6964917a2bd1df7
Petr <vystydp@gmail.com>
2024-06-05 08:37:26 +0200

    feat: hardcoded url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ClientWrapper from '../components/ClientWrapper';
 import { Event } from '../types/event'
 
 async function fetchEvents(): Promise<Event[]> {
-  const res = await fetch('http://react-events-list.vercel.app');
+  const res = await fetch('http://react-events-list.vercel.app/api/events');
   if (!res.ok) {
     throw new Error('Failed to fetch events');
   }
