a5f491e517048a1713d60f876802dfe97442141a
Petr <vystydp@gmail.com>
2024-06-05 08:40:02 +0200

    fix: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ClientWrapper from '../components/ClientWrapper';
 import { Event } from '../types/event'
 
 async function fetchEvents(): Promise<Event[]> {
-  const res = await fetch('http://react-events-list.vercel.app/api/events');
+  const res = await fetch('http://localhost:3000/api/events');
   if (!res.ok) {
     throw new Error('Failed to fetch events');
   }
