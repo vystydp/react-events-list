0e1ba97fab29e48411e9c01131c82ff47543d07a
Petr <vystydp@gmail.com>
2024-06-05 08:47:17 +0200

    fix: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import ClientWrapper from '../components/ClientWrapper';
 import { Event } from '../types/event'
 
 async function fetchEvents(): Promise<Event[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/events`);
+  var url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/events`;
+  const res = await fetch(url);
   if (!res.ok) {
     throw new Error('Failed to fetch events');
   }
