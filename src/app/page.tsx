645f1257c3f48da521d57dd3f90eefcff7eba94b
Petr <vystydp@gmail.com>
2024-06-06 12:22:01 +0200

    chore: comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import ClientWrapper from '../components/ClientWrapper';
 import { Event } from '../types/event'
 
 export default function Page() {
+  // This is actually not needed in this impl 
+  // but lets keep it for component reusability
   let initialEvents: Event[] = [];
   
   return (
